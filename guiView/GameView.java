b63b0df6b940903682a5540cfed4227e5f3e125b
Curtis <Zhuangyongj@gmail.com>
2019-05-21 16:52:40 +0100

    Create: create a new class called GameView
    A new class called GameView is created to represent the window of this
    game

---
+++ guiView/GameView.java
@@ -0,0 +1,35 @@
+package guiView;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class GameView {
+
+	//default size of this game's frame
+	private final Dimension frameDim;
+	
+	private JFrame gameFrame;
+	private JPanel contentPane;
+
+	public GameView() {
+		
+		// set up basic components
+		gameFrame = new JFrame("Herding Cats!!!!!!");
+		contentPane = new JPanel();
+		gameFrame.setContentPane(contentPane);
+		
+		// set screen size
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		frameDim = new Dimension(screenSize.width/2, screenSize.height/2);
+		gameFrame.setSize(frameDim);
+		
+		
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gameFrame.setVisible(true);
+	}
+	
+}
