694fe60a5323d13cacd8405f1e61adcb6f2deea5
Curtis <Zhuangyongj@gmail.com>
2019-05-28 00:52:53 +0100

    Update: Class dog
    - removed move method.
    - added setSquare method.
    - updated java doc for the variables and methods.

--- models/Dog.java
+++ models/Dog.java
@@ -2,6 +2,7 @@ package models;
 
 /**
  * Dog class that models every individual dog.
+ * 
  * @author Curtis
  */
 public class Dog {
@@ -15,17 +16,18 @@ public class Dog {
      * The x coordinate of the dog.
      */
     private int dogPositionX;
-    
+
     /**
      * The y coordinate of the dog.
      */
     private int dogPositionY;
-    
+
     /**
      * Constructor for Dog class.
+     * 
      * @param square the square that the dog is on.
-     * @param dogX the x coordinate of the dog.
-     * @param dogY the y coordinate of the dog.
+     * @param dogX   the x coordinate of the dog.
+     * @param dogY   the y coordinate of the dog.
      */
     public Dog(int square, int dogX, int dogY) {
 	this.squareDog = square;
@@ -34,58 +36,44 @@ public class Dog {
     }
 
     /**
-     * Move the dog to a new square. It also checks whether the new position is the
-     * squares that are adjacent to current square, because the dog is only allowed
-     * to move to the adjacent squares. The boolean value refers to whether the dog
-     * has moved. This boolean variable is used in a while loop in the main method
-     * to make sure that the dog is correctly moved to the adjacent squares.
-     * 
-     * If newSquareDog for the dog is less than 1 and larger than 9, it moves the
-     * dog to this illegitimate square. In this case, the main method will directly
-     * skip the rest of the code and display "existing!".
+     * move the dog to a new square.
      * 
      * @param newSquareDog the new square of the dog.
      */
-    public boolean move(int newSquareDog) {
-	boolean moved;
-	if (newSquareDog > 0 && newSquareDog < 10) {
-	    if (squareDog == 0) {
-		squareDog = newSquareDog;
-		moved = true;
-	    } else if (isAdjacentSquare(newSquareDog)) {
-		squareDog = newSquareDog;
-		moved = true;
-	    } else {
-		System.out.println("The dog can only move to the square adjacent!");
-		moved = false;
-	    }
-	} else {
-	    // Moves the dog to this illegitimate position.
-	    System.out.println("It's an illegitimate position!");
-	    moved = true;
-	    squareDog = newSquareDog;
-	}
-	return moved;
-    }// End - move()
+    public void setSquare(int newSquareDog) {
+	this.squareDog = newSquareDog;
+    }
 
     /**
      * Return the square that the dog is on.
+     * 
      * @return
      */
     public int getSquareDog() {
 	return squareDog;
     }
 
-    // Return dogPositionX
+    /**
+     * get dogPositionX
+     * @return x coordinate of the dog.
+     */
     public int getDogPositionX() {
 	return dogPositionX;
     }
 
-    // Return dogPositionY
+    /**
+     * get dogPositionY
+     * @return y coordinate of the dog.
+     */
     public int getDogPositionY() {
 	return dogPositionY;
     }
 
+    /**
+     * set the positions of the dog.
+     * @param newPositionX new x coordinate of the dog.
+     * @param newPositionY new y coordinate of the dog.
+     */
     public void setDogPositions(int newPositionX, int newPositionY) {
 	dogPositionX = newPositionX;
 	dogPositionY = newPositionY;
